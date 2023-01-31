a54b33e6d106eb4c6df9adff9b301bff315c42ae
sheep <sheep@sheepdev.xyz>
2023-01-31 17:25:13 +0100

    fix(gitignore) urgent

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('/home/sheep/projects/tako/docs/node_modules/infima/dist/css/default/default.css'),
-  require('/home/sheep/projects/tako/docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/home/sheep/projects/tako/docs/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/home/sheep/projects/tako/docs/src/css/custom.css'),
-];
